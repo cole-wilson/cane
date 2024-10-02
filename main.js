0b88e495713b0c238c5b118f593b9d49041068d2
Cole Wilson <colewilson.main@gmail.com>
2024-10-01 20:18:14 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+let v;
+
+v = document.getElementsByTagName("video")[0];
+
+let output = {};
+
+if (v.paused) v.play();
+
+output.network_status = v.networkState;
+output.resolution = [v.videoWidth, v.videoHeight];
+output.player_size = [parseInt(getComputedStyle(v).width.replace("px","")), parseInt(getComputedStyle(v).height.replace("px",""))]
+output.remaining_buffer = -1;
+output.n_buffers = v.buffered.length;
+output.paused = v.paused;
+output.percent = v.currentTime / v.duration;
+output.current_time = v.currentTime;
+output.video_url = location.href;
+
+for (var i=0;i<v.buffered.length;i++) {
+	if (v.currentTime > v.buffered.start(i) && v.currentTime < v.buffered.end(i)) {
+		output.remaining_buffer = v.buffered.end(i) - v.currentTime;
+		break;
+	}
+}
+
+let qual = v.getVideoPlaybackQuality()
+output.dropped_frames = qual.droppedVideoFrames;
+output.total_frames = qual.totalVideoFrames;
+output.corrupted_frames = qual.corruptedVideoFrames;
+
+return output;
---
+++ main.py
@@ -0,0 +1,62 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.by import By
+import json
+import platform
+import os
+
+MAX_BANDWIDTH = 70000 # Kbps
+MIN_BANDWIDTH = 500 # Kbps
+
+def setBandwidth(Kbps):
+    op_sys = platform.system()
+    if op_sys == "Darwin":
+        bandwidth = Kbps / 4 * 1024
+        driver.set_network_conditions(offline=False,latency=0,download_throughput=bandwidth, upload_throughput=bandwidth)
+    else:
+        bandwidth = Kbps / 1000
+        os.system(f"tc qdisc add dev eth0 root tbf rate {bandwidth}")
+
+
+
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.youtube.com/watch?v=KLuTLF3x9sA")
+# driver.get("https://vimeo.com/15298502")
+# driver.get("https://www.tbs.com/shows/the-big-bang-theory/season-7/episode-8/the-itchy-brain-simulation")
+# driver.get("https://www.c-span.org/video/?538521-1/simulcast-cbs-news-vice-presidential-debate")
+
+driver.implicitly_wait(0.5)
+
+with open("main.js", "r") as f:
+    funcjs = f.read()
+
+
+data = []
+time.sleep(2)
+body = driver.find_elements(By.TAG_NAME, "body")[0]
+body.send_keys("Space")
+
+driver.execute_script("document.getElementsByTagName('video')[0].play()")
+print('play')
+time.sleep(5)
+
+# skip_ads = WebDriverWait(driver, 20*60).until(EC.element_to_be_clickable((By.CLASS_NAME, "ytp-skip-ad-button")))
+# skip_ads.click()
+
+start = time.time()
+
+try:
+    while True:
+        seconds = time.time() - start
+        setBandwidth(1000)
+
+        output = driver.execute_script(funcjs)
+        # print(output)
+        data.append(output)
+except:
+    ...
+
+with open("data.json", "w+") as f:
+    json.dump(data, f)
