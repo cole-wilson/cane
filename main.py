51e31a5703cd793950d937fe2e6bbc3336fc7b8c
Cole Wilson <colewilson.main@gmail.com>
2024-11-22 15:24:35 -0800

    fix issue with versions

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ def setBandwidth(Kbps):
         bandwidth = Kbps
         burst = 1000
         max_latency = 1000
-        os.system(f"sudo ../wondershaper/wondershaper clear {INTERFACE}")
+        os.system(f"sudo ../wondershaper/wondershaper -c {INTERFACE}")
         os.system(f"sudo ../wondershaper/wondershaper -a {INTERFACE} -d {bandwidth}")
         # os.system(f"sudo /usr/sbin/tc qdisc add dev {INTER} root tbf rate {bandwidth}kbit burst {burst} latency {max_latency}ms")
     return Kbps
