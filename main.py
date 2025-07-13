b399064abbceb4cd9dfb1a40ff1db6ff92eacfe1
Cole Wilson <colewilson.main@gmail.com>
2025-07-13 15:00:07 -0700

    update

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def setBandwidth(Kbps):
     if op_sys == "Darwin":
         driver.set_network_conditions(offline=False,latency=0,download_throughput=bandwidth, upload_throughput=bandwidth)
     else:
-        os.system(f"sudo ../wondershaper/wondershaper -a {INTERFACE} -c") # 2> err.log
-        os.system(f"sudo ../wondershaper/wondershaper -a {INTERFACE} -d {Kbps}")
+        os.system(f"sudo ../wondershaper/wondershaper -a {INTERFACE} -c  2> err.log") #
+        os.system(f"sudo ../wondershaper/wondershaper -a {INTERFACE} -d {Kbps} 2> err.log")
 
 # def bandwidth_from_time(t):
 #     # print("\n"*10)
